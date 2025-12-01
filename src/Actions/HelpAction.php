efc2c29ff5ff676aeac35b595d12d143b28c138d
James Carlos <james@learning.net>
2025-12-01 01:04:27 -0800

    fix: alpineClickHandler syntax in HelpAction

--- src/Actions/HelpAction.php
+++ src/Actions/HelpAction.php
@@ -31,7 +31,7 @@ class HelpAction extends Action
             ->when(
                 KnowledgeBase::companion()->hasModalPreviews(),
                 fn (HelpAction $action) => $action
-                    ->alpineClickHandler('$dispatch(\"open-modal\", {id: "' . $documentable->getId() . '"})'),
+                    ->alpineClickHandler('$dispatch("open-modal", {id: "' . $documentable->getId() . '"})'),
                 fn (HelpAction $action) => $action->url($documentable->getUrl())
             )
         ;
